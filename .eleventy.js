fc8cb2623157175d97c5a70635cbade50aaea24c
bag <bag@bags-MacBook-Pro.local>
2022-10-30 19:43:09 -0400

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -5,22 +5,22 @@ const md = new markdownIt({
   linkify: true
 });
 
-// var implicitFigures = require('markdown-it-implicit-figures');
+var implicitFigures = require('markdown-it-implicit-figures');
 
-// md.use(implicitFigures, {
-//   dataType: false,  // <figure data-type="image">, default: false
-//   figcaption: false,  // <figcaption>alternative text</figcaption>, default: false
-//   tabindex: true, // <figure tabindex="1+n">..., default: false
-//   link: false // <a href="img.png"><img src="img.png"></a>, default: false
-// });
+md.use(implicitFigures, {
+  dataType: false,  // <figure data-type="image">, default: false
+  figcaption: false,  // <figcaption>alternative text</figcaption>, default: false
+  tabindex: true, // <figure tabindex="1+n">..., default: false
+  link: false // <a href="img.png"><img src="img.png"></a>, default: false
+});
 
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("assets/images");
-    // eleventyConfig.addPassthroughCopy("assets/css");
-    // eleventyConfig.addWatchTarget("assets/css");
-    // eleventyConfig.addPassthroughCopy("assets/scripts");
+    eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addWatchTarget("assets/css");
+    eleventyConfig.addPassthroughCopy("assets/scripts");
     eleventyConfig.addFilter("markdown", (content) => {
       return md.render(content);
     });
