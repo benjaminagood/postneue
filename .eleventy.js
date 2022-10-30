b14aeb5c5786900d9f85ba3b7ad1e6bafdcfd681
bag <bag@bags-MacBook-Pro.local>
2022-10-30 19:06:08 -0400

    clean up

--- .eleventy.js
+++ .eleventy.js
@@ -18,9 +18,9 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("assets/images");
-    eleventyConfig.addPassthroughCopy("assets/css");
-    eleventyConfig.addWatchTarget("assets/css");
-    eleventyConfig.addPassthroughCopy("assets/scripts");
+    // eleventyConfig.addPassthroughCopy("assets/css");
+    // eleventyConfig.addWatchTarget("assets/css");
+    // eleventyConfig.addPassthroughCopy("assets/scripts");
     // eleventyConfig.addFilter("markdown", (content) => {
     //   return md.render(content);
     // });
