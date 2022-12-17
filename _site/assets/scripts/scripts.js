cb6274ff539a0428261754f188b5d8007da25ea8
bag <bag@bags-MacBook-Pro.local>
2022-12-17 15:28:56 -0600

    color data

--- _site/assets/scripts/scripts.js
+++ _site/assets/scripts/scripts.js
@@ -2,3 +2,15 @@ $("header a").click(function(){
     $(this).toggleClass( "active" );
     $("#about").toggle();
   });
+
+
+  // $("li").hover(function(){
+  //   var color = $(this).data();
+  //   console.log(color);
+  //   // $("body").css("background-color", value);
+  // });
+
+//   $("li").hover(function() {
+//     var newColor = $(this).data("bgcolor");
+//     $("body").css("background-color", newColor);
+// })	
