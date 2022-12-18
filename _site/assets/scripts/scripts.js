612f8a4159530a2253065998359f96f3f3f87367
bag <bag@bags-MacBook-Pro.local>
2022-12-17 20:34:30 -0600

    config and styling

--- _site/assets/scripts/scripts.js
+++ _site/assets/scripts/scripts.js
@@ -2,15 +2,3 @@ $("header a").click(function(){
     $(this).toggleClass( "active" );
     $("#about").toggle();
   });
-
-
-  // $("li").hover(function(){
-  //   var color = $(this).data();
-  //   console.log(color);
-  //   // $("body").css("background-color", value);
-  // });
-
-//   $("li").hover(function() {
-//     var newColor = $(this).data("bgcolor");
-//     $("body").css("background-color", newColor);
-// })	
