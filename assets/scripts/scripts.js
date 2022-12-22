8f1987509d04f8dab28979749b1e6c617d5e069c
bag <bag@bags-MacBook-Pro.local>
2022-12-21 18:12:24 -0600

    index grid changes

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,22 @@
 $("header a").click(function(){
     $(this).toggleClass( "active" );
-    $("#about").toggle();
+    $(".infobox").toggle();
   });
+
+
+
+  $( ".gallery figure" ).click(function() {
+
+    var nextSlide = $(this).next();
+
+    $(this).css("display", "none");
+
+    if( nextSlide.length == 0 ) {
+         // nextSlide = $(this).prevAll('.slide').last();
+         $(".gallery figure").first().css("display", "block");
+
+     }
+    else {
+         $(this).next().css("display", "block");
+    }
+    });
